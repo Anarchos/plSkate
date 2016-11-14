4349354928b11e7b338684c2804166ecb608e73c
Adam <adstermoore@gmail.com>
2016-11-14 16:51:53 +0000

    Added skate, initialise and query functions.
    Added a question, a very simple starting UI and the ability for the user to answer the question.

--- skate.pl
+++ skate.pl
@@ -1,3 +1,27 @@
 /*------------------------*/
 /*-Insert Epic Title Here-*/
 /*------------------------*/
+
+skate:-
+
+	initialise,
+	query.
+
+
+
+initialise:-
+	nl,
+	tab(20), write('<><><><><><><>'),nl,
+	tab(20), write('<><>SKATE!<><>'),nl,
+	tab(20), write('<><><><><><><>'),nl.
+
+query:-
+	question(Quest, Known),
+	write(Quest), nl,
+	/*insert validation function*/
+	read(X), nl,	%replace this when vaildation function is added.
+	X='y' ->	%if they answer yes.
+	write('They answered yes');	%else do this.
+	write('They answered no'), nl.
+
+question("Do you own a skateboard?", Owns_Board).
